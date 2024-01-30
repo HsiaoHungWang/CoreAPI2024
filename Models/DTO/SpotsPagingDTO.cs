a96299598894efcae8558ff4d42c2ab49ddc684c
hhwang <wang@ispan.com.tw>
2024-01-30 23:07:15 +0800

    加入專案檔案。

---
+++ Models/DTO/SpotsPagingDTO.cs
@@ -0,0 +1,11 @@
+﻿using CoreAPI2024.Models;
+
+namespace CoreAPI2024.Models.DTO
+{
+    public class SpotsPagingDTO
+    {
+        public int TotalPages { get; set; }
+       // public int TotalCount { get; set; }
+        public List<SpotImagesSpot>? SpotsResult { get; set; }
+    }
+}
